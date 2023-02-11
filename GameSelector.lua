79fb527003fa39af8f662362deac7ffd052023fc
super5gamer46 <78188828+super5gamer46@users.noreply.github.com>
2023-02-11 12:31:08 -0800

    Update GameSelector.lua

--- GameSelector.lua
+++ GameSelector.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 5194438573 then
+if game.PlaceId == 4032944086 then
     -- MAIN
     loadstring(game:HttpGet('https://raw.githubusercontent.com/super5gamer46/ScriptHub/main/GetHugeSimulator'))()
         end
